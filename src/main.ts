bb7396dfb2262f6cab2a6fe8a5a0acf2df264614
Kabira K. <kabira@monallabs.in>
2024-03-10 09:45:31 +0530

    reproduce safelist uitlity class error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
 import './app.postcss';
-import App from './App.svelte';
-import { justpyComponents, setJustpyComponents } from './store_jpcomponents';
 import SkeletonUI from './SkeletonUI.svelte';
 
 
@@ -10,8 +8,4 @@ import { storePopup, setInitialClassState } from '@skeletonlabs/skeleton';
 setInitialClassState()
 storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow });
 
-// const app = new SkeletonUI({
-//     target: document.getElementById('app') as Element
-//  });
-
-export {App, SkeletonUI, justpyComponents, setJustpyComponents};
+export {SkeletonUI};
--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import postcss from './postcss.config.ts';
 export default defineConfig({
    plugins: [svelte(),
    purgeCss({
-			safelist: ['btn-icon', 'variant-filled', 'fa-solid', 'fa-skull'],
+			safelist: ['badge', 'btn-icon', 'variant-filled', 'fa-solid', 'fa-skull'],
 		}
 		)
    ],
