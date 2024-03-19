f2fdb076e0b1ec324a9a72af247372007c817091
Kabira K. <kabira@monallabs.in>
2024-03-19 12:51:22 +0530

    bug with <svelte:element

---
+++ src/ComponentRenderByType.svelte
@@ -0,0 +1,16 @@
+<script>
+   import SkeletonComponent from './SkeletonComponent.svelte';
+   
+   let components =  {
+		    'skeleton_component': SkeletonComponent
+                    
+                    };
+   export let jp_props;
+		  
+</script>
+
+<svelte:component this={components[jp_props.vue_type]} jp_props={jp_props} />
+
+
+
+
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import './app.postcss';
+import App from './App.svelte';
+import { justpyComponents, setJustpyComponents } from './store_jpcomponents';
+
 import SkeletonSafelist from './SkeletonSafelist.svelte';
 
-export {SkeletonSafelist};
+export {App,  SkeletonSafelist,  justpyComponents, setJustpyComponents};
