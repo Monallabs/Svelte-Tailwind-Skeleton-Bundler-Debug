467ac4cc0792315e4abe48f3d3780367fb312267
Kabira K. <kabira@monallabs.in>
2024-03-19 13:03:51 +0530

    added SkeletonComponent

---
+++ src/SkeletonComponent.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let jp_props;
+    import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+    
+  const components = {
+    accordion: Accordion,
+  };
+
+  const descriptionObject = {
+    ...jp_props.attrs,
+    style: jp_props.style,
+    class: jp_props.classes, 
+  };
+
+ const selected_component = components[jp_props.html_tag];
+ 
+</script>
+
+<svelte:element this=Accordion {...descriptionObject}></svelte:element>
+<svelte:element this={selected_component} {...descriptionObject}></svelte:element>
+
+
