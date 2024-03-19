d5419801ec6da77c720b3c20b4f2a4e5077b2782
Kabira K. <kabira@monallabs.in>
2024-03-19 13:02:52 +0530

    added App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+import ComponentRenderByType from './ComponentRenderByType.svelte';
+   import { justpyComponents, setJustpyComponents } from './store_jpcomponents';
+   $: console.log('Store value:', $justpyComponents);
+		  
+</script>
+
+
+{#each $justpyComponents as jpcomp}
+   <svelte:component this={ComponentRenderByType} jp_props={jpcomp}/>
+{/each}
+
